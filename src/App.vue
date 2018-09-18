3ca9411616e3a1bb2546494ff5d98a38c0ab18a9
lizbetmendoza <lizbetmendoza@users.noreply.github.com>
2018-09-18 17:54:03 -0400

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,97 @@
+<template>
+  <div id="app">
+    <div class="navbar has-shadow">
+      <div class="container ">
+        <div class="navbar-start">
+          <a class="navbar-item">My portafolio</a>
+        </div>
+
+        <span class="navbar-burger" v-on:click="toggleNav" v-bind:class="{'is-active': isActive}">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+
+        <div class="navbar-end navbar-menu" v-bind:class="{'is-active': isActive }">
+          <router-link to="/" class="navbar-item r-item">Home</router-link>
+          <router-link to="/" class="navbar-item r-item">AboutMe</router-link>
+          <router-link to="/" class="navbar-item r-item">ContactMe</router-link>
+
+        </div>
+      </div>
+    </div>
+    <router-view></router-view>
+
+    <footer class="footer is-primary">
+      <div class="container">
+        <div class="columns">
+          <div class="columns">
+            <p>This is an space, where you can put information about footer</p>
+          </div>
+          <div class="column has-text-right">
+            <a class="icon" href="#"><i class="fa fa-facebook"></i></a>
+            <a class="icon" href="#"><i class="fa fa-twitter"></i></a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data: function(){
+    return {
+      isActive: false
+    }
+  },
+  methods: {
+    toggleNav: function() {
+      this.isActive =!this.isActive; 
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+@import '../node_modules/bulma/bulma.sass'
+@import 'mq'
+
+
+.navbar 
+  background-color: #383838
+  a hover
+    color: gray
+
+.navbar-start a
+  color: #fff
+  font-weight:bold
+
+a.r-item
+  color: #C1C1C1
+  padding: 0.5rem 1.75rem
+  +mobile
+      color: gray
+      &:hover
+        background-color: #F1F1F1
+
+.navbar-end a
+  
+  font-weight:bold
+
+.navbar-burger span
+  background-color: #F1F1F1
+
+footer
+  background-color: $primary !important
+  color: #fff
+
+  .icon
+    color: #fff
+    margin-left: 20px
+
+    .icon hover
+      color: red
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="home">
+    <section class="hero">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">{{ heading }}</h1>
+          <div class="is-two-thirds column is-paddingless">
+            <h2 class="subtitle is-2 has-text-centered">{{ subheading }}</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="section">
+      <div class="container">
+        <div class="columns pd is-desktop">
+          <!--<div class="column is-1 has-text-centered">    
+            <i class="fa fa-cog is-primary"></i>
+          </div>-->
+          <div class="column has-text-centered"> <!-- is-one-third-desktop -->
+            <!--<p class="title"><strong>We provide superior logistics so that your business can succeed in a crazy world.</strong></p>-->
+            <img src="../assets/me.svg">
+          </div>
+          <div class="column has-text-centered">
+            <p class="title is-1 is-primary">Hi, my name is Liz<br>I am a Web Developer</p><br>
+            <p class="subtitle is-4">I am interested in pursuing a career in software development<br> I have experience as a web developer,<br> I consider myself a fast learner,<br> involved in different projects and <br>complete project cycles, testing, <br>and final implementations.</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="columns pd">
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="subtitle">- Technical Skills</p>
+              <p class="subtitle">C#. Net, ASP.Net, PHP, jQuery, jQuery Mobile,	JavaScript, Adobe Photoshop.<br>
+	              Databases:	 MySQL, SQL server
+                Webserver: 	XAMPP control (cross-platform), AWS Amazon Web Services 
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="subtitle">Web Sites</p>
+              <p class="subtitle">"I think it's an absolutely excellent tool for our business. I can't survive without this thing."</p>
+            </div>
+          </div>
+        </div>
+        <div class="column">
+          <div class="card">
+            <div class="card-content">
+              <p class="subtitle">GitHub</p>
+              <p class="subtitle">"I think it's an absolutely excellent tool for see my playground :-)<br>
+                 I can't survive without this thing."</p>
+              
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      heading: 'This is me :-)',
+      subheading: '...and I love what I do'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+  @import '../mq'
+
+  .hero
+    background: url('../assets/header.png')
+    background-size: cover
+
+  .title
+    +mobile
+      font-weight: bold
+    +tablet
+      font-size: 2.5rem
+    +desktop
+      font-size: 4rem
+      margin-top: 2rem
+  h1
+    margin: 1.5rem 0 2rem 18rem !important
+  h2
+    margin: 1.5rem 0 2rem 0 !important
+
+  .fa-cog
+    font-size: 40px
+
+  #learn
+    +desktop
+      margin-bottom: 2rem
+
+  .pd
+    +tablet
+      padding: 2em 0
+
+</style>
